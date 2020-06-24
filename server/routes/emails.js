bea1506d54a9ba3b60f6ef6a48403744960b0950
run <run27017@126.com>
2020-06-24 16:23:37 +0800

    实现邮件模板发送的方式

--- server/routes/emails.js
+++ server/routes/emails.js
@@ -2,6 +2,7 @@ const _ = require('lodash')
 const express = require('express')
 const { body, validationResult } = require('express-validator')
 const EmailDao = require('../dao/email')
+const TemplateDAO = require('../dao/template')
 const websocket = require('../websocket')
 const { stripHTMLTags } = require('../../lib/htmltools')
 
@@ -39,7 +40,7 @@ router.get('/:id', async function(req, res, next) {
   res.send({ email })
 })
 
-router.post('/', [
+router.post('/send', [
   body('email.toAddress').not().isEmpty(),
   body('email.fromAddress').not().isEmpty(),
   body('email.subject').not().isEmpty(),
@@ -60,6 +61,32 @@ router.post('/', [
   })
 })
 
+router.post('/xsend', [
+  body('email.toAddress').not().isEmpty(),
+  body('email.fromAddress').not().isEmpty(),
+  body('email.templateId').not().isEmpty(),
+  body('email.tags').isArray()
+], async function(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  const emailParams = _.pick(req.body.email, ['toAddress', 'toName', 'fromAddress', 'fromName', 'tags', 'subject'])
+  const { templateId, bindings } = req.body.email
+  const { subject, content, contentType } = await parseContent(templateId, bindings)
+  emailParams.content = content
+  emailParams.type = contentType
+  if (!emailParams.subject) emailParams.subject = subject
+
+  const email = await EmailDao.create(emailParams)
+  res.status(201).send({ email })
+  websocket.broadcast({
+    event: 'NewEmail',
+    data: email
+  })
+})
+
 function simplifyContent (emails) {
   emails.forEach(email => {
     if (email.type === 'text') {
@@ -68,6 +95,15 @@ function simplifyContent (emails) {
       email.content = stripHTMLTags(email.content).substr(0, 80)
     }
   })
+}
+
+// TODO: email 和 message 都用了此方法
+async function parseContent (templateId, bindings) {
+  const template = await TemplateDAO.find(templateId)
+  const content = template.content.replace(/%{([^{}]+)}/g, function (_, variable) {
+    return bindings[variable]
+  })
+  return { subject: template.subject, content, contentType: template.contentType }
 }
 
 module.exports = router
