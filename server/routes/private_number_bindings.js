d7be6bda4a421ac5fee70b3cefeb4c683fe30878
Run <run27017@126.com>
2021-08-06 19:41:49 +0800

    添加面向开发者的绑定虚拟号码 API

--- server/routes/private_number_bindings.js
+++ server/routes/private_number_bindings.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { param, body } = require('express-validator')
 const PrivateNumberBindingDAO = require('../dao/private_number_binding')
 
 const router = express.Router()
@@ -8,14 +9,32 @@ router.get('/', async function(req, res, _next) {
   const perPage = parseInt(req.query.perPage || '10')
   
   const { privateNumberBindings, pagination } = await PrivateNumberBindingDAO.all({ currentPage, perPage })
-  res.send({ privateNumberBindings, total: pagination.total, pagination })
+  res.send({ privateNumberBindings, pagination })
 })
 
-router.get('/:id', async function(req, res, _next) {
-  const id = parseInt(req.params.id)
-  
-  const privateNumberBinding = await PrivateNumberBindingDAO.one(id)
-  res.send({ privateNumberBinding })
+router.get('/:id', [
+  param('id').toInt()
+], async function(req, res, _next) {
+  const binding = await PrivateNumberBindingDAO.one(req.params.id)
+  res.send({ privateNumberBinding: binding })
 })
+
+router.post('/', [
+  body('privateNumberBinding.phoneNumberA').notEmpty(),
+  body('privateNumberBinding.phoneNumberB').notEmpty()
+], async function(req, res, _next) {
+  const bindingParams = req.body.privateNumberBinding
+  const virtualNumber = generateVirtualNumber(bindingParams.phoneNumberA, bindingParams.phoneNumberB)
+
+  const binding = await PrivateNumberBindingDAO.create({ ...bindingParams, virtualNumber  })
+  res.send({ privateNumberBinding: binding })
+})
+
+function generateVirtualNumber (phoneNumberA, phoneNumberB) {
+  const cutNumberA = phoneNumberA.substr(0, 4).padStart(4, '0') 
+  const cutNumberB = phoneNumberB.substr(0, 4).padStart(4, '0')
+  const randomNumber = Math.floor(Math.random() * 1000).toString().padStart(4, '0')
+  return cutNumberA + cutNumberB + randomNumber
+}
 
 module.exports = router
